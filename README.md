8de3d48c5d6946df5ba946dc1da20ccb4324b4f9
noeljr2015 <72898090+noeljr2015@users.noreply.github.com>
2024-11-11 22:33:47 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# CFB-Game-Predictor
-Machine learning that predicts the outcome of any Division I college football game. Data are from 2015 - 2024 seasons. My DNN has an accuracy of 84% on the validation data. The model will use multi-class learning to use the prior week's feature data to predict next week's feature data. Multiple models were used to achieve this. Data are from SportsReference and CFBD. 
+# College Football Game Predictions
+
+machine learning that predicts the outcome of any Division I college football game. Data are from 2015 - 2024 seasons.
+My DNN has an accuracy of 84% on the validation data. I use multi-class learning to use the prior week's feature data
+to predict next week's feature data. I used multiple models to achieve this. Data are from [SportsReference](https://www.sports-reference.com/cfb/) and 
+[CFBD](https://collegefootballdata.com/). The .pkl files I could not upload to GitHub due to size issues.
+
+THE BIG PROBLEM:
+-this is trained on every college football teams. The models have bias from the bad teams, thus every top 25 team
+it assums that they are just going to win every game, as their feature values are substantially higher than the bad teams.
+Therefore, when a bad team wins with "low" feature scores, and a top 25 team wins with "high" feature scores, the model assumes 
+then that if the good team for a week plays bad, they will still win.
+
+## Usage
+
+```bash
+python3 collect_augment_data.py #update the data for 2024 every week
+python3 deep_learning_multiclass.py test #evaluate the model on the "test" data, which is the top 25 teams last week's outcomes
+python3 deep_learning_multiclass.py notest #Predict the outcomes between two teams
+```
+### Current prediction accuracies
+![](https://github.com/bszek213/deepCFB/blob/main/Training.png) 
+
+### Outputs
+example out from the `results.txt`
+```bash
+==============================
+Win Probabilities from Monte Carlo Simulation with 10000 simulations
+louisiana-lafayette : 99.999 %buffalo : 0.001 %
+Win Probabilities from rolling median of 2 predictions
+louisiana-lafayette : 100.0 %buffalo : 0.0 %
+Win Probabilities from rolling median of 3 predictions
+louisiana-lafayette : 99.99 %buffalo : 0.01 %
+Win Probabilities from exponential weighted average of 2 predictions
+louisiana-lafayette : 100.0 %buffalo : 0.0 %
+Win Probabilities from 25th and 75th percentile rolling 2
+25th: louisiana-lafayette : 100.0 %buffalo : 0.0 %
+75th: louisiana-lafayette : 99.999 %buffalo : 0.001 %
+==============================
+```
+
+### My Simple Rating System
+I created a simple rating system that uses the median point differential of one team and subtracts that value
+by all median point differential of all other teams they played against. The only caveat is that I only count 
+the point differentials of the other teams when they lose, as I want to be able to assess how much each team is 
+losing by. The theory behind this is that good teams will not lose often and when they do it will most likely be 
+by small margins. The higher the srs value, the "better" the team is.
+![](https://github.com/bszek213/deepCFB/blob/main/my_srs.png) 
+
+### Creating CFBD api key
+got to [CFBD](https://collegefootballdata.com/key) and enter your email address. they will send you an API key. 
+Create a file called `api_key.yaml` and store the API key like this:
+```bash
+api_key:
+  Authorization: asdifnasdofnasdpvnapionmfaspidfasodnfkajdslmalskdm
+```
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
---
+++ simple_rating_system.py
@@ -0,0 +1,156 @@
+#Simple rating systme to assess how "good" a team is
+
+from collect_data import data_for_srs
+from collect_augment_data import augment_team_names
+from numpy import median, absolute
+from math import isnan
+from pandas import DataFrame, read_csv
+from tqdm import tqdm
+from matplotlib.pyplot import show, xticks, tight_layout, savefig
+import argparse
+from os.path import exists, join
+from colorama import Fore, Style
+import yaml
+from os import getcwd
+"""
+Rating = Team's Average Point Differential - Strength of Schedule
+and then rank each team
+strength of schedule is the opposing team's point differential
+"""
+
+def get_df(team):
+    if team == 'tcu':
+        team = 'texas-christian'
+    if team == 'lafayette':
+        team = 'louisiana-lafayette'
+    if team == 'utsa':
+        team = 'texas-san-antonio'
+    if team == 'utep':
+        team = 'texas-el-paso'
+    if team == 'sam-houston':
+        team = "sam-houston-state"
+    if team == 'louisiana':
+        team = 'louisiana-lafayette'
+    str_combine = 'https://www.sports-reference.com/cfb/schools/' + team + '/' + str(2024) + '/gamelog/'
+    team, opps  = data_for_srs(str_combine,team,2024)
+    return team, opps
+
+def teams_to_test():
+    with open(join(getcwd(),'team_rankings_year.yaml')) as file:
+            teams_dict_year = yaml.load(file, Loader=yaml.FullLoader)
+    teams_list = teams_dict_year[2024]
+    return teams_list
+
+def get_pt_diff_team_1(df):
+    df[['team_1_score', 'team_2_score']] = df['game_result'].str.extract(r'(\d+)-(\d+)').astype(int)
+    df['differential'] = df['team_1_score'] - df['team_2_score']
+    return df['differential'].median()
+
+def fix_school_names(input_list):
+    processed_school_names = []
+    for name in input_list:
+        # Remove parentheses and ampersands
+        cleaned_name = name.replace("(", "").replace(")", "").replace("&", "")
+        lowercase_name = cleaned_name.lower()
+        processed_name = lowercase_name.replace(" ", "-")
+        if processed_name == 'tcu':
+            processed_name = 'texas-christian'
+        if processed_name == 'lafayette':
+            processed_name = 'louisiana-lafayette'
+        if processed_name == 'utsa':
+            processed_name = 'texas-san-antonio'
+        # Append the processed name to the new list
+        processed_school_names.append(processed_name)
+    return processed_school_names
+
+def get_pt_diff_team_2(team):
+    team[['team_1_score', 'team_2_score']] = team['game_result'].str.extract(r'(\d+)-(\d+)').astype(int)
+    team['differential'] = team['team_1_score'] - team['team_2_score']
+    #may attempt to help improve on SRS - only include games that team lost to detract from team_1's overall pt diff
+    update_values = team['differential'][team['differential'] < 0].to_numpy()
+    return median(absolute(update_values))
+
+def main():
+    parser = argparse.ArgumentParser(description="SRS - my version")
+    parser.add_argument('--all', type=str, choices=['yes', 'no'], help='Perform a teams_to_test() function when "yes" is provided, otherwise if no, input team names.')
+    parser.add_argument('--team_1', type=str, help='Name of team 1')
+    parser.add_argument('--team_2', type=str, help='Name of team 2')
+    args = parser.parse_args()
+    #save outputs
+    team_dict = {}
+    if args.all == 'yes':
+        team_output_list = (teams_to_test())
+    else:
+        team_output_list = []
+        # Input the two teams of interests
+        team_output_list.append(args.team_1)#input('input team_1: ')
+        team_output_list.append(args.team_2)#input('input team_2: ')
+    if not exists('my_srs.csv') and args.all == 'yes':
+        for teams in tqdm(team_output_list):
+            print(f'current team: {teams}')
+            team, opps = get_df(teams)
+            team1_pt_diff = get_pt_diff_team_1(team)
+            processed_school_names = fix_school_names(opps)
+            # print(processed_school_names)
+            opp_team_averages = []
+            for opp_team in processed_school_names:
+                try:
+                    team, opps = get_df(opp_team)
+                    opp_diff = float(get_pt_diff_team_2(team))
+                    # print(f'{opp_team}: {opp_diff}')
+                    if isnan(opp_diff):
+                        opp_diff = 0
+                    # print(f'{opp_team}: {opp_diff}')
+                    opp_team_averages.append(opp_diff)
+                except:
+                    print(f'{opp_team} does not not have data. Check spelling or some teams do not have data')
+
+            #Calc SRS
+            srs = team1_pt_diff - median(opp_team_averages)
+            team_dict[teams] = srs
+            print(team_dict)
+        #df and sorting
+        final_df = DataFrame({'Teams': list(team_dict.keys()), 'SRS': list(team_dict.values())})
+        sorted_teams = final_df.sort_values(by='SRS', ascending=False)
+        if args.all == 'yes':
+            sorted_teams.to_csv('my_srs.csv',index=False)
+        print('=======================')
+        print(f'SRS output')
+        print(team_dict)
+        print('=======================')
+    else:
+        sorted_teams = read_csv('my_srs.csv')
+        for teams in team_output_list:
+            print(f'current team: {teams}')
+            team, opps = get_df(teams)
+            team1_pt_diff = get_pt_diff_team_1(team)
+            processed_school_names = fix_school_names(opps)
+            # print(processed_school_names)
+            opp_team_averages = []
+            for opp_team in processed_school_names:
+                try:
+                    team, opps = get_df(opp_team)
+                    opp_diff = float(get_pt_diff_team_2(team))
+                    # print(f'{opp_team}: {opp_diff}')
+                    if isnan(opp_diff):
+                        opp_diff = 0
+                    # print(f'{opp_team}: {opp_diff}')
+                    opp_team_averages.append(opp_diff)
+                except:
+                    print(f'{opp_team} does not not have data. Check spelling or some teams do not have data')
+
+            #Calc SRS
+            srs = team1_pt_diff - median(opp_team_averages)
+            team_dict[teams] = srs
+        print(Fore.YELLOW + Style.BRIGHT + '=======================')
+        print(f'Two teams SRS output')
+        print(team_dict)
+        print('=======================' + Style.RESET_ALL)
+    if args.all == 'yes':
+        sorted_teams.plot.bar(x='Teams',y='SRS',figsize=(20, 6))
+        tight_layout()
+        xticks(rotation=45)
+        savefig('my_srs.png',dpi=400)
+
+if __name__ == "__main__":
+    main()
